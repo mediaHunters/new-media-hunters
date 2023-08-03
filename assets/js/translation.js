0b5eb043f60f235a53ec6b9c22cfd7dc3bb5dbbb
adiyy2001 <adiy@onet.pl>
2023-08-03 17:18:52 +0200

    update tranlsations paritially

---
+++ assets/js/translation.js
@@ -0,0 +1,18 @@
+import * as pl from "./i18n/pl.js";
+
+console.log(pl.default);
+document.addEventListener("DOMContentLoaded", () => {
+  i18next.init({
+    lng: "pl",
+    debug: true,
+    resources: {
+      pl: pl.default,
+    },
+  });
+
+  const elementsToTranslate = document.querySelectorAll("[data-i18n]");
+  elementsToTranslate.forEach((element) => {
+    const translationKey = element.dataset.i18n;
+    element.textContent = i18next.t(translationKey);
+  });
+});
